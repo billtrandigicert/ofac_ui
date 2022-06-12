649b737557ecb1f70fdf2b1fa15511dd1a74e52a
Bill Tran <bill.tran@DC-W7464V3KGW.lan>
2022-06-12 18:30:13 -0500

    Update download feature

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request, Response
 import flask_excel as excel
 from datetime import timedelta
@@ -16,34 +17,30 @@ def view_list():
                            disposition=get_dispositions(), ofac=view_data(from_date=last_week, to_date=latest_date))
 
 
-@app.route('/filtered_list')
+@app.route('/filtered_list', methods=["POST"])
 def filter_list():
-    from_date = request.args.get("from")
-    to_date = request.args.get("to")
-    return render_template("index.html", date=get_date(), order_no=get_orders(), list_match=get_match(),
-                           disposition=get_dispositions(), ofac=view_data(from_date=from_date, to_date=to_date))
+    from_date = request.form.get("from")
+    to_date = request.form.get("to")
+    if request.form['action'] == 'Run Report':
+        return render_template("index.html", date=get_date(), order_no=get_orders(), list_match=get_match(),
+                            disposition=get_dispositions(), ofac=view_data(from_date=from_date, to_date=to_date))
+    elif request.form['action'] == 'Click here to download!':
+        ofac = view_data(from_date=from_date, to_date=to_date)
+        ofac = [{'order_id': record['order_id'], 'cert_serial_number': record['cert_serial_number'],
+                'disposition': record['disposition']} for record in ofac]
+
+        excel.init_excel(app)
+        extension_type = "csv"
+        filename = "ofactory_legal_disposition" + "." + extension_type
+        return excel.make_response_from_records(ofac, file_type=extension_type, file_name=filename)
 
 
 @app.route('/update_list', methods=['POST'])
 def update_list():
-    pk = request.form['pk']
-    value = request.form['value']
+    pk = request.form.get('pk')
+    value = request.form.get('value')
     update_db(pk, value)
     return json.dumps({'status': 'OK'})
-
-
-@app.route('/download_csv')
-def download_csv():
-    from_date = request.args.get("from")
-    to_date = request.args.get("to")
-    ofac = view_data(from_date='2019-06-06', to_date='2022-06-07')
-    ofac = [{'order_id': record['order_id'], 'cert_serial_number': record['cert_serial_number'],
-             'disposition': record['disposition']} for record in ofac]
-
-    excel.init_excel(app)
-    extension_type = "csv"
-    filename = "ofactory_legal_disposition" + "." + extension_type
-    return excel.make_response_from_records(ofac, file_type=extension_type, file_name=filename)
 
 
 if __name__ == '__main__':
